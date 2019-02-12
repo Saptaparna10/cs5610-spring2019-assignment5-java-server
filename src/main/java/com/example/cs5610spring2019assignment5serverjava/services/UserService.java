e434112200a7bc0f2917439bab601b244f65b946
Saptaparna10 <saptaparna1991@gmail.com>
2019-02-12 16:10:47 -0500

    profile

--- src/main/java/com/example/cs5610spring2019assignment5serverjava/services/UserService.java
+++ src/main/java/com/example/cs5610spring2019assignment5serverjava/services/UserService.java
@@ -4,11 +4,14 @@ import java.util.*;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.cs5610spring2019assignment5serverjava.models.Person;
 
+@RestController
 public class UserService {
 	
 	List<Person> users = new ArrayList<Person>();
@@ -20,5 +23,12 @@ public class UserService {
 		users.add(user);
 		return user;
 	}
+	
+	@GetMapping("/api/profile")
+	public Person profile(HttpSession session) {
+		Person currentUser = (Person) session.getAttribute("currentUser");	
+		return currentUser;
+	}
+
 
 }
