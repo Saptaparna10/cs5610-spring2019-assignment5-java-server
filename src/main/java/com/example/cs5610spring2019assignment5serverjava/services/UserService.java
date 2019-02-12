280f8b1a58ac255097c1e2ff9b8ab939d0d519b3
Saptaparna10 <saptaparna1991@gmail.com>
2019-02-12 16:00:43 -0500

    sign up implemented

--- src/main/java/com/example/cs5610spring2019assignment5serverjava/services/UserService.java
+++ src/main/java/com/example/cs5610spring2019assignment5serverjava/services/UserService.java
@@ -1,5 +1,24 @@
 package com.example.cs5610spring2019assignment5serverjava.services;
 
+import java.util.*;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.cs5610spring2019assignment5serverjava.models.Person;
+
 public class UserService {
+	
+	List<Person> users = new ArrayList<Person>();
+	
+	@PostMapping("/api/register")
+	public Person register(@RequestBody Person user,
+	HttpSession session) {
+		session.setAttribute("currentUser", user);
+		users.add(user);
+		return user;
+	}
 
 }
