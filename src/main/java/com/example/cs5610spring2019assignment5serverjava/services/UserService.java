88ec1103dae7c1a0c57ed2c4f97134f917cbef74
Saptaparna10 <saptaparna1991@gmail.com>
2019-02-12 16:26:41 -0500

    find user implemented

--- src/main/java/com/example/cs5610spring2019assignment5serverjava/services/UserService.java
+++ src/main/java/com/example/cs5610spring2019assignment5serverjava/services/UserService.java
@@ -5,6 +5,7 @@ import java.util.*;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,23 +14,23 @@ import com.example.cs5610spring2019assignment5serverjava.models.Person;
 
 @RestController
 public class UserService {
-	
+
 	List<Person> users = new ArrayList<Person>();
-	
+
 	@PostMapping("/api/register")
 	public Person register(@RequestBody Person user,
-	HttpSession session) {
+			HttpSession session) {
 		session.setAttribute("currentUser", user);
 		users.add(user);
 		return user;
 	}
-	
+
 	@GetMapping("/api/profile")
 	public Person profile(HttpSession session) {
 		Person currentUser = (Person) session.getAttribute("currentUser");	
 		return currentUser;
 	}
-	
+
 	@PostMapping("/api/logout")
 	public void logout(HttpSession session) {
 		session.invalidate();
@@ -37,15 +38,29 @@ public class UserService {
 
 	@PostMapping("/api/login")
 	public Person login(	@RequestBody Person credentials,
-	HttpSession session) {
-	 for (Person user : users) {
-	  if( user.getUsername().equals(credentials.getUsername())
-	   && user.getPassword().equals(credentials.getPassword())) {
-	    session.setAttribute("currentUser", user);
-	    return user;
-	  }
-	 }
-	 return null;
+			HttpSession session) {
+		for (Person user : users) {
+			if( user.getUsername().equals(credentials.getUsername())
+					&& user.getPassword().equals(credentials.getPassword())) {
+				session.setAttribute("currentUser", user);
+				return user;
+			}
+		}
+		return null;
+	}
+
+	@GetMapping("/api/users")
+	List<Person> findAllUsers() {
+		return users;
+	}
+
+	@GetMapping("/api/user/{id}")
+	Person findUserById(@PathVariable("id") int id) {
+		for(Person user : users)
+			if(user.getId() == id)
+				return user;
+		return null;
+
 	}
 
 }
