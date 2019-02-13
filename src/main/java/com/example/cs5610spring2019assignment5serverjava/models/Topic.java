72b4576c3ad665356bd917a8f32c3d52d0074eef
Saptaparna10 <saptaparna1991@gmail.com>
2019-02-12 20:10:18 -0500

    topic service implemented

--- src/main/java/com/example/cs5610spring2019assignment5serverjava/models/Topic.java
+++ src/main/java/com/example/cs5610spring2019assignment5serverjava/models/Topic.java
@@ -25,5 +25,37 @@ public class Topic {
 	public void setWidgets(List<Widget> widgets) {
 		this.widgets = widgets;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((widgets == null) ? 0 : widgets.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Topic other = (Topic) obj;
+		if (id != other.id)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (widgets == null) {
+			if (other.widgets != null)
+				return false;
+		} else if (!widgets.equals(other.widgets))
+			return false;
+		return true;
+	}
 	
 }
--- src/main/java/com/example/cs5610spring2019assignment5serverjava/services/TopicService.java
+++ src/main/java/com/example/cs5610spring2019assignment5serverjava/services/TopicService.java
@@ -1,5 +1,115 @@
 package com.example.cs5610spring2019assignment5serverjava.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cs5610spring2019assignment5serverjava.models.Course;
+import com.example.cs5610spring2019assignment5serverjava.models.Lesson;
+import com.example.cs5610spring2019assignment5serverjava.models.Module;
+import com.example.cs5610spring2019assignment5serverjava.models.Topic;
+
+@RestController
 public class TopicService {
+	
+	@Autowired
+	LessonService ls;
+	
+	@Autowired
+	ModuleService ms;
+	
+	@Autowired
+	CourseService cs;
+	
+	@PostMapping("/api/lessons/{lid}/topics")
+	public Topic createTopic(
+			@PathVariable("lid") int lid,
+			@RequestBody Topic topic) {
+
+		Lesson lesson = ls.findLessonById(lid);
+
+		if(lesson!=null) {
+			topic.setId(IdGenerator.generateId(TopicService.class));
+			if(lesson.getTopics()==null)
+				lesson.setTopics(new ArrayList<>());
+			lesson.getTopics().add(topic);
+			return topic;
+		}
+		return null;
+	}
+	
+	@GetMapping("/api/lessons/{lid}/topics")
+	public List<Topic> findAllTopics(@PathVariable("lid") int lid){
+		Lesson lesson = ls.findLessonById(lid);
+		if(lesson!=null) {
+			return lesson.getTopics();
+		}
+		return null;
+	}
+
+	@GetMapping("/api/topics/{tid}")
+	public Topic findTopicById(@PathVariable("tid") int tid){
+		List<Course> courses = cs.findAllCourses();
+		if(courses==null)
+			return null;
+		
+		for(Course course: courses) {
+			for(Module module: course.getModules()) {
+				for(Lesson lesson: module.getLessons()) {
+					for(Topic topic: lesson.getTopics()) {
+						if(topic.getId()==tid)
+							return topic;
+					}
+				}
+			}
+		}
+		return null;
+	}
+	
+	@PutMapping("/api/topics/{tid}")
+	public Topic updateTopic(@PathVariable("tid") int tid, 
+			@RequestBody Topic topic) {
+		Topic top= findTopicById(tid);
+		if(top!=null) {
+			top.setWidgets(topic.getWidgets());
+			top.setTitle(topic.getTitle());
+			return top;
+		}
+		return null;
+	}
+
+	@DeleteMapping("/api/topics/{tid}")
+	public List<Topic> deleteTopic
+	(@PathVariable("tid") int tid) {
+
+		List<Course> courses = cs.findAllCourses();
+		if(courses==null)
+			return null;
+
+		for(Course course: courses) {
+			for(Module module: course.getModules()) {
+				for(Lesson lesson: module.getLessons()) {
+					for(Topic topic: lesson.getTopics()) {
+						if(topic.getId()==tid) {
+							lesson.getTopics().remove(topic);
+							return lesson.getTopics();
+						}
+					}
+				}
+			}
+		}
+
+		return null;
+	}
+
+
 
 }
